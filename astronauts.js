00452dc66d957be60ad3112dc966a0ddfbcc0f22
Nasdon33 <carlo.1994@live.it>
2018-01-12 10:18:51 +0100

    Inserisco gestione inserimento e lettura astronauti

--- astronauts.js
+++ astronauts.js
@@ -3,6 +3,8 @@ var bodyParser = require('body-parser');
 var JsonDB = require('node-json-db');
 const astronauts = express.Router();
 
+var uuid = require('uuid/v4');
+
 var db = new JsonDB("json/db.json", true, false);
 
 
@@ -11,9 +13,30 @@ var db = new JsonDB("json/db.json", true, false);
 astronauts.route('/')
     .post(function(req,res){
         // Creare un astronauta
+        const newAstronaut = req.body;
+        newAstronaut.astronautID = uuid();
+        newAstronaut.lastModified = new Date();
+        db.push("/database/data[]", newAstronaut);
+        res.json(newAstronaut);
     })
     .get(function(req, res){
         // Visualizzare tutti o alcuni astronauti
+        var Astronauts = [];
+        var i = 0;
+        while(true)
+        {
+            var data;
+            try
+            {
+                data = db.getData("/database/data[" + i + "]");
+            }
+            catch(error)
+            {
+                break;
+            }
+            Astronauts.push(data);
+        }
+        res.json(Astronauts);
     });
 
 
